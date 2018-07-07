9d83d7a404f71fb3b40f5fbbb2b6e24a7021d70d
tanawat <tanawat_klomklom@cmu.ac.th>
2018-07-07 18:08:39 +0700

    main

---
+++ app/src/main/java/th/co/banana/doorlock/base/BaseActivity.java
@@ -0,0 +1,54 @@
+package th.co.banana.doorlock.base;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    public void addFragment(int containerId, Fragment fragment){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(containerId, fragment)
+                .addToBackStack(null)
+                .commit();
+    }
+
+    public void replaceFragmentAddBackStack(int containerId, Fragment fragment){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(containerId, fragment)
+                .addToBackStack(String.valueOf(getSupportFragmentManager().getBackStackEntryCount() + 1))
+                .commit();
+    }
+
+    public void replaceFragment(int containerId, Fragment fragment){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(containerId, fragment)
+                .commit();
+    }
+
+    public void removeFragment(int containerId, Fragment fragment){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .remove(fragment)
+                .commit();
+    }
+
+    public void back(){
+        if (getSupportFragmentManager().getBackStackEntryCount() > 1){
+            getSupportFragmentManager().popBackStack();
+        }else {
+            finish();
+        }
+    }
+
+    public void backToMain(){
+        finish();
+    }
+
+    public void toastMessage(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+    }
+}
